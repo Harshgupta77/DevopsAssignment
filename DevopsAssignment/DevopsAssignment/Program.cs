5c7e2267528324740db8680251636c3c94ebd47f
Harshgupta77 <48287995+Harshgupta77@users.noreply.github.com>
2023-04-19 18:30:05 +0530

    Add files via upload

---
+++ DevopsAssignment/DevopsAssignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DevopsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
